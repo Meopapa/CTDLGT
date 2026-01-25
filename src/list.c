51a035bb288faef178f5c36cb3e0421347abf479
Nguyen Nhat Minh <mn4546615@gamil.com>
2026-01-25 16:23:26 +0700

    DAY 2: Done linear data struture

---
+++ src/list.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "include\dsa_configs.h"
+#include "include\data_structure.h"
+#include "include\list.h"
+
+#define LIST_IMPLEMENT_INIT(type1, type2, type3, head) \
+    int LIST_Init_##type1##_##type2##_##type3() \
+    { \
+        head = NULL; \
+    } \
+    int LIST_Free_##type1##_##type2##_##type3() \
+    { \
+        List_##type1##_##type2##_##type3 *l; \
+        while(head != NULL) \
+        { \
+            l = head; \
+            head = head->next_ptr; \
+            head->pre_ptr = NULL; \
+            free(l); \
+        } \
+    } 
+#define LIST_IMPLEMENT_INSERT(type1, type2, type3, head) \
+    int LIST_InsertFront_##type1##_##type2##_##type3(List_##type1##_##type2##_##type3 *l) \
+    { \
+        if(l == NULL) return ERROR_NULL_PTR; \
+        if(head == NULL) head = l; \
+        else \
+        { \
+            head->pre_ptr = l; \
+            l->next_ptr = head; \
+            head = l; \
+        } \
+        return SUCCESS_TRUE; \
+    } \
+    int LIST_InsertBack_##type1##_##type2##_##type3(List_##type1##_##type2##_##type3 *l) \
+    { \
+        if(l == NULL) return ERROR_NULL_PTR; \
+        if(head == NULL) return ERROR_EMPTY; \
+        while(head->next_ptr != NULL) head = head->next_ptr; \
+        head->next_ptr = l; \
+        l->pre_ptr = head; \
+    } \
+    int LIST_InsertMiddle_##type1##_##type2##_##type3(type1 *m1, type2 *m2, type3 *m3, List_##type1##_##type2##_##type3 *l_insert);
+#define LIST_IMPLEMENT_DELETE(type1, type2, type3, head) \
+    int LIST_DeleteFront_##type1##_##type2##_##type3() \
+    { \
+        List_##type1##_##type2##_##type3 *l; \
+        l = head; \
+        head = head->next_ptr; \
+        free(l); \
+        return SUCCESS_TRUE; \
+    } \
+    int LIST_DeleteBack_##type1##_##type2##_##type3() \
+    { \
+        while(head->next_ptr->next_ptr != NULL) head = head->next_ptr; \
+        free(head->next_ptr); \
+        head->next_ptr = NULL; \
+    } \
+    int LIST_DeleteMiddle_##type1##_##type2##_##type3(type1 *m1, type2 *m2, type3 *m3);
+#define LIST_IMPLEMENT_TRAVERSE() \
+    int LIST_Traverse_##type1##_##type2##_##type3(); \
